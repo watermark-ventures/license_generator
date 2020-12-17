f62625d2e36ceaa85d24d76fd4adfa09f5a8276b
William Fullerton <wrk.fullerton@gmail.com>
2020-12-17 13:56:42 -0600

    added app.js / package.json / license template

---
+++ app.js
@@ -0,0 +1,47 @@
+const generateLicense = require("./utils/generateLicense")
+var inquirer = require("inquirer")
+const fs = require("fs")
+
+const questions = [
+    // Creator's Name
+    {
+        type: "input",
+        message: "What is the name of the name of the licensee?",
+        name: "licensee_name"
+    },
+
+    // Licensors's Name
+    {
+        type: "input",
+        message: "What is the name of the licensor?",
+        name: "licensor_name"
+    },
+
+    // What is the title of the work being licensed?
+    {
+        type: "input",
+        message: "What is the title of the work that is being licensed?",
+        name: "title"
+    },
+
+    // Price paid to license the work
+    {
+        type: "input",
+        message: "What is the price/fee paid in order to license the work?",
+        name: "price"
+    }   
+];
+
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+        fs.writeFile("./licenses/LICENSE.txt", generateLicense(answers), function (err) {
+            if (err) {
+                throw err;
+            }
+        })
+    })
+}
+
+init();
